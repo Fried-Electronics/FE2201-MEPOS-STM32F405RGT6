2996788c8e8bc80033e58ba68df14c0207a18e29
Fried Electronics <tom.fry@friedelectronics.co.uk>
2022-08-30 21:01:13 +0100

    Started command buffer code

--- STM32 Software/ASCII Parser/Core/Inc/ASCII_Parser.h
+++ STM32 Software/ASCII Parser/Core/Inc/ASCII_Parser.h
@@ -8,8 +8,21 @@
 #ifndef INC_ASCII_PARSER_H_
 #define INC_ASCII_PARSER_H_
 
-extern uint16_t HeadRxBufferHS;						// Head of the Rx buffer
-extern uint16_t TailRxBufferHS;						// Tail of the Rx buffer
+/*			Defines			*/
+#define MAX_NUMBER_OF_COMMANDS 64
+#define COMMAND_MAX_LENGTH 64
+
+/*			Types			*/
+typedef struct __CommandTypeDef
+{
+	char Command_String[COMMAND_MAX_LENGTH];
+	uint16_t Command_Length;
+	uint16_t Enum;
+} CommandTypeDef;
+
+/*			Exported Variables			*/
+extern uint16_t HeadRxBufferHS;	// Head of the Rx buffer
+extern uint16_t TailRxBufferHS;	// Tail of the Rx buffer
 
 /*			Exported Functions			*/
 void Increment_Head (uint32_t *Length);
--- STM32 Software/ASCII Parser/Core/Src/ASCII_Parser.c
+++ STM32 Software/ASCII Parser/Core/Src/ASCII_Parser.c
@@ -9,11 +9,14 @@
 #include "main.h"
 #include "usbd_cdc_if.h"
 #include "ASCII_Parser.h"
-
+#include "string.h"
 
 /*			Private Variables			*/
 uint16_t HeadRxBufferHS = 0;	// Head of the Rx buffer
 uint16_t TailRxBufferHS = 0;	// Tail of the Rx buffer
+
+CommandTypeDef CommandBuffer[MAX_NUMBER_OF_COMMANDS];
+uint16_t CommandBufferCount = 0;
 
 /*			Function Prototypes			*/
 
@@ -54,4 +57,51 @@ uint8_t Check_Data_Available (void)
 	}
 
 	return DataAvailable;
+}
+
+/**
+  * @brief  Adds a new command to the command buffer
+  * @param	String: String for the command
+  * @param	Enum: Enum value to be returned when this command is matched when parsed
+  * @retval -1 for error, 1 for no error
+  */
+int8_t New_Command (char *String, uint16_t Enum)
+{
+	int8_t ReturnValue = 1; // No error
+	CommandTypeDef Command;
+
+	if (CommandBufferCount >= MAX_NUMBER_OF_COMMANDS)
+	{
+		ReturnValue = -1;
+	}
+
+	if (ReturnValue != -1)
+	{
+		strcpy(Command.Command_String, String);
+		Command.Command_Length = strlen(String);
+		Command.Enum = Enum;
+
+		CommandBuffer[CommandBufferCount++] = Command;
+	}
+	return ReturnValue;
+}
+
+/**
+  * @brief  Searches the command buffer for the string
+  * @param  String: String of the new incoming command
+  * @retval -1 for invalid command, any other value for a match
+  */
+int16_t Search_Command_Buffer (char *String)
+{
+	int8_t ReturnEnum = -1;
+
+	for (int i = 0; i < CommandBufferCount; i++)
+	{
+		if (strncmp(String, CommandBuffer[i].Command_String, CommandBuffer[i].Command_Length) == 0)
+		{
+			ReturnEnum = CommandBuffer[i].Enum;
+			break;
+		}
+	}
+	return ReturnEnum;
 }
